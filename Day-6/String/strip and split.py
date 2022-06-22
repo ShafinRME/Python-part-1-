d963b95d1c0a7e6c8b144a7d851f50827a073a0c
ShafinRME <shafin.rmedu4@gmail.com>
2022-06-22 21:44:58 +0600

    Strip, upper lower, capitalize and split is done

---
+++ Day-6/String/strip and split.py
@@ -0,0 +1,15 @@
+country = "  Hi i am Shafin Ahmed  "
+print(country.lstrip())
+print(country.rstrip())
+print(country.strip())
+print(country)
+
+name = "shafin AHMED"
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+
+blogs = "Hi i am shafin ahmed, i am a web-developer"
+print(blogs.split())
+for blog in blogs.split():
+    print(blog)
