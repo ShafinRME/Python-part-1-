a4f1b994d978718ec4d41fa58532f8170017df62
ShafinRME <shafin.rmedu4@gmail.com>
2022-06-22 21:31:41 +0600

    Replace and find method is done

---
+++ Day-6/String/find and replace.py
@@ -0,0 +1,10 @@
+# country = "Bangladesh"
+# print(country.find("Bang"))
+# print(country.find("bang"))
+# print(country.find("esh"))
+# print(country.find("esi"))
+
+country = "Bangladesh"
+new_country = country.replace("Bangla", "English")
+print(new_country)
+print(country)
